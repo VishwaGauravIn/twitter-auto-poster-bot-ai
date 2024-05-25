0f78745339a7be8956cd81fcccaf0d4ecc79ad08
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-05-25 19:43:44 +0530

    Script + flow

---
+++ index.js
@@ -0,0 +1,46 @@
+// By VishwaGauravIn (https://itsvg.in)
+
+const GenAI = require("@google/generative-ai");
+const { TwitterApi } = require("twitter-api-v2");
+const SECRETS = require("./SECRETS");
+
+const twitterClient = new TwitterApi({
+  appKey: SECRETS.APP_KEY,
+  appSecret: SECRETS.APP_SECRET,
+  accessToken: SECRETS.ACCESS_TOKEN,
+  accessSecret: SECRETS.ACCESS_SECRET,
+});
+
+const generationConfig = {
+  maxOutputTokens: 400,
+};
+const genAI = new GenAI.GoogleGenerativeAI(SECRETS.GEMINI_API_KEY);
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({
+    model: "gemini-pro",
+    generationConfig,
+  });
+
+  // Write your prompt here
+  const prompt =
+    "generate a web development content, tips and tricks or something new or some rant or some advice as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+  sendTweet(text);
+}
+
+run();
+
+async function sendTweet(tweetText) {
+  try {
+    await twitterClient.v2.tweet(tweetText);
+    console.log("Tweet sent successfully!");
+  } catch (error) {
+    console.error("Error sending tweet:", error);
+  }
+}
