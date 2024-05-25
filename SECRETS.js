945bf91b521d19aadede9094503d8e7074991683
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-05-25 22:16:37 +0530

    added comments

--- SECRETS.js
+++ SECRETS.js
@@ -1,3 +1,6 @@
+//? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
+//? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
+
 const APP_KEY = "YOUR_TWITTER_APP_KEY";
 const APP_SECRET = "YOUR_TWITTER_APP_SECRET";
 const ACCESS_TOKEN = "YOUR_TWITTER_ACCESS_TOKEN";
